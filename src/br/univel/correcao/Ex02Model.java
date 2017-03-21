b445619ad2410a87a78b0e8b633f7ae9034ae824
jmattana <jhonatan_mattana@outlook.com>
2017-03-20 21:42:46 -0300

    criando construtor Ex02Model

--- src/br/univel/correcao/Ex02Model.java
+++ src/br/univel/correcao/Ex02Model.java
@@ -7,9 +7,19 @@ public class Ex02Model extends AbstractTableModel {
 	private static final long serialVersionUID = 1L;
 	private int d1 = 1, d2 = 4, d3 = 8, d4 = 3, d5 = 1, d6 = 5;
 	
+	
+	
+	public Ex02Model(String ra) {
+		d1 = Integer.parseInt(ra.substring(0 ,1));
+		d2 = Integer.parseInt(ra.substring(0 ,4));
+		d3 = Integer.parseInt(ra.substring(0 ,3));
+		d4 = Integer.parseInt(ra.substring(0 ,1));
+		d5 = Integer.parseInt(ra.substring(0 ,5));
+	}
+
 	@Override
 	public int getColumnCount() {
-		
+
 		return 0;
 	}
 
